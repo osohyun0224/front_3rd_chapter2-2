9bc09afd1bfcdcfdfbd2ad13d96c9b3f8ebdf2b1
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-10-20 14:48:01 +0900

    ♻️  refactor :: useProducts 훅 내부에서 이미 등록된 상품 처리 알람 형식으로 리팩토링

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -4,23 +4,23 @@ import { Product } from "../../types.ts";
 export const useProducts = (initialProducts: Product[]) => {
   const [products, setProducts] = useState<Product[]>(initialProducts);
 
+  const addProduct = (newProduct: Product) => {
+    setProducts((prevProducts) => {
+      const exists = prevProducts.some(product => product.id === newProduct.id);
+      if (exists) {
+        alert("이 ID를 가진 상품이 이미 존재합니다.");
+        return prevProducts;
+      }
+      return [...prevProducts, newProduct];
+    });
+  };
+
   const updateProduct = (updatedProduct: Product) => {
     setProducts((prevProducts) => 
       prevProducts.map((product) => 
         product.id === updatedProduct.id ? updatedProduct : product
       )
     );
-  };
-
-  const addProduct = (newProduct: Product) => {
-    setProducts((prevProducts) => {
-      const exists = prevProducts.some(product => product.id === newProduct.id);
-      if (exists) {
-        console.error("이 ID를 가진 상품이 이미 존재해 상품을 등록할 수 없습니다.", newProduct.id);
-        return prevProducts;
-      }
-      return [...prevProducts, newProduct];
-    });
   };
 
   return {
