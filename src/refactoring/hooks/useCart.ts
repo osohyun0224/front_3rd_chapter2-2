94c31b07f9b9ed4579b457ab31befd54b1d3b8d6
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-10-20 14:34:56 +0900

    ✨ feat :: useCart 훅 내부 addToCart 함수 구현

--- src/refactoring/hooks/useCart.ts
+++ src/refactoring/hooks/useCart.ts
@@ -1,4 +1,3 @@
-// useCart.ts
 import { useState } from 'react';
 import { CartItem, Coupon, Product } from '../../types';
 import { calculateCartTotal, updateCartItemQuantity } from './utils/cartUtils';
@@ -7,7 +6,25 @@ export const useCart = () => {
   const [cart, setCart] = useState<CartItem[]>([]);
   const [selectedCoupon, setSelectedCoupon] = useState<Coupon | null>(null);
 
-  const addToCart = (product: Product) => {};
+  const addToCart = (product: Product) => {
+    setCart((prevCart) => {
+      const index = prevCart.findIndex(item => item.product.id === product.id);
+
+
+      if (index !== -1) {
+        return prevCart.map((item, idx) =>
+          idx === index
+            ? {
+                ...item,
+                quantity: Math.min(item.quantity + 1, product.stock)
+              }
+            : item
+        );
+      } else {
+        return [...prevCart, { product, quantity: 1 }];
+      }
+    });
+  };
 
   const removeFromCart = (productId: string) => {};
 
