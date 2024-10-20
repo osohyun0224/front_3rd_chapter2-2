57ea5705c5908530d47e0b02c0518654ca295c59
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-10-20 14:42:39 +0900

    ✨ feat :: useCoupon 훅 내부 addCoupon 함수 구현

--- src/refactoring/hooks/useCoupon.ts
+++ src/refactoring/hooks/useCoupon.ts
@@ -1,6 +1,19 @@
-import { Coupon } from "../../types.ts";
 import { useState } from "react";
+import { Coupon } from "../../types.ts";
 
 export const useCoupons = (initialCoupons: Coupon[]) => {
-  return { coupons: [], addCoupon: () => undefined };
+  const [coupons, setCoupons] = useState<Coupon[]>(initialCoupons);
+
+  const addCoupon = (newCoupon: Coupon) => {
+    setCoupons((prevCoupons) => {
+      const exists = prevCoupons.some(coupon => coupon.code === newCoupon.code);
+      if (exists) {
+        console.error("이미 쿠폰이 존재해 새로 등록할 수 없습니다.", newCoupon.code);
+        return prevCoupons;
+      }
+      return [...prevCoupons, newCoupon];
+    });
+  };
+
+  return { coupons, addCoupon };
 };
