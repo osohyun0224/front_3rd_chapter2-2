9ee4856ab576ddb9acf67cb2dd35a4a2029290d5
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-10-20 16:03:20 +0900

    📝 docs :: useCoupons 훅에 대한 jsdoc 작성

--- src/refactoring/hooks/useCoupon.ts
+++ src/refactoring/hooks/useCoupon.ts
@@ -1,6 +1,14 @@
 import { useState } from "react";
 import { Coupon } from "../../types.ts";
 
+/**
+ * @function useCoupons
+ * @description 초기 쿠폰 목록을 기반으로 쿠폰 상태를 관리하는 커스텀 훅
+ *
+ * @param {Coupon[]} initialCoupons - 초기에 설정할 쿠폰 목록
+ * @returns {object} 쿠폰 관리 객체
+ */
+
 export const useCoupons = (initialCoupons: Coupon[]) => {
   const [coupons, setCoupons] = useState<Coupon[]>(initialCoupons);
 
