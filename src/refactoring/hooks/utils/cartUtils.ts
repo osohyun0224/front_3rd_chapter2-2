05e23613598bfe54063f7d61a4c55332da3f383a
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-10-20 14:31:00 +0900

    ✨ feat :: calculateItemTotal 함수와 getMaxApplicableDiscount 함수 구현

--- src/refactoring/hooks/utils/cartUtils.ts
+++ src/refactoring/hooks/utils/cartUtils.ts
@@ -1,21 +1,17 @@
-import { CartItem, Coupon } from "../../../types";
+import { CartItem } from "../../../types";
 
-export const calculateItemTotal = (item: CartItem) => {
-  return 0;
+export const calculateItemTotal = (item: CartItem, applyDiscount: boolean = true) => {
+  const { product: { price }, quantity } = item;
+  const discountMultiplier = applyDiscount ? (1 - getMaxApplicableDiscount(item)) : 1;
+  return price * quantity * discountMultiplier;
 };
+
 
 export const getMaxApplicableDiscount = (item: CartItem) => {
-  return 0;
+  const { discounts } = item.product;
+  const { quantity } = item;
+  return discounts.reduce((max, discount) => (
+    quantity >= discount.quantity ? Math.max(max, discount.rate) : max
+  ), 0);
 };
 
-export const calculateCartTotal = (cart: CartItem[], selectedCoupon: Coupon | null) => {
-  return {
-    totalBeforeDiscount: 0,
-    totalAfterDiscount: 0,
-    totalDiscount: 0,
-  };
-};
-
-export const updateCartItemQuantity = (cart: CartItem[], productId: string, newQuantity: number): CartItem[] => {
-  return []
-};
