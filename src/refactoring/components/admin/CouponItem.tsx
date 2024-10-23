87eaa598db5a778b2586365e4cdccf9074cdbfda
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-10-24 01:58:37 +0900

    ✨ feat(CouponItem) :: UI-CouponItem 컴포넌트 구현

--- src/refactoring/components/admin/CouponItem.tsx
+++ src/refactoring/components/admin/CouponItem.tsx
@@ -1,16 +1,15 @@
-import { FC } from 'react';
-import { Coupon } from '../../../types';
-
+import { FC, ReactNode } from 'react';
 interface CouponItemProps {
-  coupon: Coupon;
-  index: number;
+  content: ReactNode;
+  testId: string;
+  className?: string;
 }
 
-export const CouponItem: FC<CouponItemProps> = ({ coupon, index }) => {
+export const CouponItem: FC<CouponItemProps> =({ content, testId, className }) => {
   return (
-    <div key={index} data-testid={`coupon-${index + 1}`} className="bg-gray-100 p-2 rounded">
-      {coupon.name} ({coupon.code}):
-      {coupon.discountType === 'amount' ? `${coupon.discountValue}원` : `${coupon.discountValue}%`} 할인
+    <div data-testid={testId} className={`bg-gray-100 p-2 rounded ${className || ''}`}>
+      {content}
     </div>
   );
 };
+
