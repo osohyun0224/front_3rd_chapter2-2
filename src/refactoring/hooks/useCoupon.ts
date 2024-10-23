0fec16206cb53127728118ede9e8f23922c6b7b8
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-10-23 20:52:18 +0900

    ♻️ refactor(useCoupon) :: useCoupon 훅 useCallback으로 감싸고, 파일명과 일치하게 훅명 리팩토링

--- src/refactoring/hooks/useCoupon.ts
+++ src/refactoring/hooks/useCoupon.ts
@@ -1,18 +1,17 @@
-import { useState } from "react";
-import { Coupon } from "../../types.ts";
-
+import { useState, useCallback } from 'react'
+import { Coupon } from '../../types'
 /**
- * @function useCoupons
+ * @function useCoupon
  * @description 초기 쿠폰 목록을 기반으로 쿠폰 상태를 관리하는 커스텀 훅
  *
  * @param {Coupon[]} initialCoupons - 초기에 설정할 쿠폰 목록
  * @returns {object} 쿠폰 관리 객체
  */
 
-export const useCoupons = (initialCoupons: Coupon[]) => {
+export const useCoupon = (initialCoupons: Coupon[]) => {
   const [coupons, setCoupons] = useState<Coupon[]>(initialCoupons);
 
-  const addCoupon = (newCoupon: Coupon) => {
+  const addCoupon = useCallback((newCoupon: Coupon) => {
     setCoupons((prevCoupons) => {
       const exists = prevCoupons.some(coupon => coupon.code === newCoupon.code);
       if (exists) {
@@ -21,7 +20,7 @@ export const useCoupons = (initialCoupons: Coupon[]) => {
       }
       return [...prevCoupons, newCoupon];
     });
-  };
+  },[]);
 
   return { coupons, addCoupon };
 };
