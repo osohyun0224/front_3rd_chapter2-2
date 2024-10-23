15cc0a7f46f39fa3a91a684b5b49eeaeaa2a8eb5
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-10-23 20:36:12 +0900

    ✨ feat(useManageCoupons) :: 쿠폰을 전체적으로 관리하는 커스텀 훅 생성

---
+++ src/refactoring/hooks/useManageCoupons.ts
@@ -0,0 +1,34 @@
+import { useCallback, useState } from 'react'
+import { Coupon } from '../../types'
+import {isValidCoupon} from './utils'
+
+type CouponManageProps = {
+  onCouponAdd: (newCoupon: Coupon) => void
+}
+
+export const useManageCoupons = ({ onCouponAdd }: CouponManageProps) => {
+  const [newCoupon, setNewCoupon] = useState<Coupon>({
+    name: '',
+    code: '',
+    discountType: 'percentage',
+    discountValue: 0,
+  })
+
+  const handleChangeCoupon = useCallback(<T extends React.ChangeEvent<HTMLInputElement | HTMLSelectElement>>(e: T) => {
+    const { name, value } = e.target
+    setNewCoupon((prev) => ({ ...prev, [name]: value }))
+  }, [])
+
+  const handleAddCoupon = useCallback(() => {
+    if (!isValidCoupon(newCoupon)) return;
+
+    onCouponAdd(newCoupon);
+    setNewCoupon({ name: '', code: '', discountType: 'percentage', discountValue: 0 });
+  }, [newCoupon, onCouponAdd]);
+
+  return {
+    newCoupon,
+    handleChangeCoupon,
+    handleAddCoupon,
+  }
+}
