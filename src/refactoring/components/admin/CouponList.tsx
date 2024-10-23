4b084636abff2de3c07f78b2d2a5ba72ad1bc7ef
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-10-24 01:59:23 +0900

    ✨ feat(CouponList) :: UI - CouponList 컴포넌트 구현

---
+++ src/refactoring/components/admin/CouponList.tsx
@@ -0,0 +1,24 @@
+import { FC } from 'react';
+import { ItemListTemplate } from '../templates';
+import { Coupon } from '../../../types';
+import { CouponItem } from './CouponItem';
+import { TitleLabel } from "../atoms";
+
+type CouponListProps = {
+  coupons: Coupon[];
+};
+
+export const CouponList: FC<CouponListProps> = ({ coupons }) => {
+  return (
+    <ItemListTemplate>
+      <TitleLabel level={'h3'}>현재 쿠폰 목록</TitleLabel>
+      {coupons.map((coupon, index) => (
+        <CouponItem
+          key={index}
+          testId={`coupon-${index + 1}`}
+          content={`${coupon.name} (${coupon.code}): ${coupon.discountType === 'amount' ? `${coupon.discountValue}원` : `${coupon.discountValue}%`} 할인`}
+        />
+      ))}
+    </ItemListTemplate>
+  );
+};
