fe9b513bb254c54d9bbb01e6f820f6be24eafbaa
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-10-24 12:06:41 +0900

    ✨ feat(ItemListTemplate) :: 아이템 목록을 감싸는 템플릿 구현

--- src/refactoring/components/templates/ItemListTemplate.tsx
+++ src/refactoring/components/templates/ItemListTemplate.tsx
@@ -1,9 +1,9 @@
-import { ReactNode } from 'react'
+import { FC, ReactNode } from 'react'
 
 type ItemListProps = {
   children: ReactNode
 }
-export const ItemListTemplate = ({ children }: ItemListProps) => {
+export const ItemListTemplate : FC<ItemListProps> = ({ children }: ItemListProps) => {
   return <div className="space-y-2">
     {children}</div>
 }
