b5cb8f5aef67206e131996c806f9c28b557129cc
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-10-24 16:21:30 +0900

    🚀 feat :: 컴포넌트 폴더별 파일 내보내기 (여러개 내보내기 관리)

--- src/refactoring/components/atoms/index.ts
+++ src/refactoring/components/atoms/index.ts
@@ -1,5 +1,6 @@
 export { Button } from './Button';
 export { Select } from './Select';
 export { Label } from './Label';
+export { LabelButton } from './LabelButton';
 export { Input } from './Input';
 export { TitleLabel } from './TitleLabel'
