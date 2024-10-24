2b0191fe153c91cbe1792a7f9d097ef673c958d7
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-10-24 10:13:53 +0900

    ✨ feat(CardTemplate) :: 카드를 감싸는 레이아웃 템플릿 구현

---
+++ src/refactoring/components/templates/CardTemplate.tsx
@@ -0,0 +1,15 @@
+import { FC, ReactNode } from 'react'
+
+type CardTemplateProps = {
+  title?: string
+  children: ReactNode
+}
+
+export const CardTemplate: FC<CardTemplateProps> = ({ title, children }) => {
+  return (
+    <div className="mt-4 mb-4 bg-white p-4 rounded shadow">
+      {title && <h2 className="text-xl font-semibold mb-2">{title}</h2>}
+      {children}
+    </div>
+  )
+}
