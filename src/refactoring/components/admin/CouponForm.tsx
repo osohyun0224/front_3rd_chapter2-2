10e0460d5c35b43bffa6bf5cf10ff6c4a1d094e0
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-10-24 11:31:23 +0900

    ✨ feat(CouponForm) :: 쿠폰을 생성하는 폼 컴포넌트 구현

---
+++ src/refactoring/components/admin/CouponForm.tsx
@@ -0,0 +1,64 @@
+import { Coupon } from '../../../types'
+import { ItemListTemplate } from '../templates'
+import { FC } from 'react'
+import { Button, Input } from '../atoms'
+import { Select } from "../atoms"
+
+type CouponFormProps = {
+  newCoupon: Coupon
+  onChangeCoupon: (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => void
+  onClickAddCoupon: () => void
+}
+
+export const CouponForm: FC<CouponFormProps> = ({ newCoupon, onChangeCoupon, onClickAddCoupon }) => {
+  const isDisabledSubmit = !newCoupon.name || !newCoupon.code || !newCoupon.discountValue;
+
+  return (
+    <ItemListTemplate>
+      <Input
+        type="text"
+        placeholder="쿠폰 이름"
+        name="name"
+        value={newCoupon.name}
+        onChange={onChangeCoupon}
+      />
+
+      <Input
+        type="text"
+        placeholder="쿠폰 코드"
+        name="code"
+        value={newCoupon.code}
+        onChange={onChangeCoupon}
+      />
+
+      <div className="flex gap-2">
+        <Select
+          name="discountType"
+          value={newCoupon.discountType}
+          onChange={onChangeCoupon}
+          options={[
+            { value: 'amount', label: '금액(원)' },
+            { value: 'percentage', label: '할인율(%)' }
+          ]}
+        />
+
+        <Input
+          type="number"
+          placeholder="할인 값"
+          name="discountValue"
+          value={newCoupon.discountValue.toString()}
+          onChange={onChangeCoupon}
+        />
+      </div>
+
+      <Button
+        color={isDisabledSubmit ? 'disabled' : 'success'}
+        text="쿠폰 추가"
+        className="w-full mt-2"
+        size="large"
+        onClick={onClickAddCoupon}
+        disabled={isDisabledSubmit}
+      />
+    </ItemListTemplate>
+  );
+};
