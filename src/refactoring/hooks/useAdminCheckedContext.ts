d7ff26dd6fa7f00b5f178b00b2353961448ef558
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-10-24 13:57:18 +0900

    ✨ feat :: useAdminCheckedContext 훅 예외 처리 추가

--- src/refactoring/hooks/useAdminCheckedContext.ts
+++ src/refactoring/hooks/useAdminCheckedContext.ts
@@ -1,11 +1,12 @@
 import { Context, useContext } from 'react'
 import { AdminContext } from '../context'
 
-export const useContextProvider = <T>(contextType: Context<T | undefined>) => {
+export const useContextProvider = <T>(contextType: Context<T | undefined>, message: string) => {
   const context = useContext(contextType)
+  if (!context) throw new Error(message)
   return context
 }
 
 export const useAdminCheckedContext = () => {
-  return useContextProvider(AdminContext)
+  return useContextProvider(AdminContext, '다른 곳에서 사용하지 못합니다.')
 }
