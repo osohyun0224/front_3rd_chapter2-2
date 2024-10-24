422d685332ec8b84e1805b995e1f738eda3deaab
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-10-24 12:09:28 +0900

    ✨ feat(SectionTemplate) :: 페이지별 섹션을 나누는 템플릿 구현

---
+++ src/refactoring/components/templates/SectionTemplate.tsx
@@ -0,0 +1,15 @@
+import { FC, ReactNode } from 'react'
+
+type SectionTemplateProps = {
+  title: string
+  children: ReactNode
+}
+
+export const SectionTemplate: FC<SectionTemplateProps> = ({ title, children }) => {
+  return (
+    <div>
+      <h2 className="text-2xl font-semibold mb-4">{title}</h2>
+      {children}
+    </div>
+  )
+}
