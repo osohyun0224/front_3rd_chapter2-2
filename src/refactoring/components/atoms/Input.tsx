64bda4565cdb514ef6869ee0c7793eab84a4deec
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-10-24 11:53:11 +0900

    ♻️ refactor ::  [디자인시스템] Input 컴포넌트에 대해서 다양한 스타일 적용 리팩토링

--- src/refactoring/components/atoms/Input.tsx
+++ src/refactoring/components/atoms/Input.tsx
@@ -4,13 +4,42 @@ interface InputProps {
   type: 'text' | 'number';
   value: string | number;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  id: string;
+  id?: string;
   name?: string;
   placeholder?: string;
+  size?: 'small' | 'medium' | 'large';
   className?: string;
 }
 
-export const Input: FC<InputProps> = ({ id, name, placeholder, type, value, onChange, className = 'w-full p-2 border rounded' }) => {
+export const Input: FC<InputProps> = ({
+  id,
+  name,
+  placeholder,
+  type,
+  value,
+  onChange,
+  size = 'medium',
+  className = ''
+}) => {
   const inputValue = type === 'number' ? value.toString() : value;
-  return <input id={id} name={name} placeholder={placeholder} type={type} value={inputValue} onChange={onChange} className={className} />;
+
+  const sizeClass = {
+    small: 'w-1/3 p-2 border rounded',
+    medium: 'w-full p-2 border rounded',
+    large: 'w-full p-4 border rounded'
+  };
+
+  const finalClassName = `${sizeClass[size]} ${className}`.trim();
+
+  return (
+    <input
+      id={id}
+      name={name}
+      placeholder={placeholder}
+      type={type}
+      value={inputValue}
+      onChange={onChange}
+      className={finalClassName}
+    />
+  );
 };
