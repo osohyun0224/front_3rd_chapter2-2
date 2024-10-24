34fe90fbf5f680586e819b45c0da1ed3425844d4
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-10-24 10:27:04 +0900

    ✨ feat(LabelButton) :: [디자인시스템] 라벨 형태의 버튼 컴포넌트 구현

---
+++ src/refactoring/components/atoms/LabelButton.tsx
@@ -0,0 +1,19 @@
+import { FC } from 'react';
+
+interface LabelButtonProps {
+  onClick: () => void;
+  label: string;
+  testId: string;
+}
+
+export const LabelButton: FC<LabelButtonProps> = ({ onClick, label, testId }) => {
+  return (
+    <button
+      data-testid={testId}
+      onClick={onClick}
+      className="w-full text-left font-semibold"
+    >
+      {label}
+    </button>
+  );
+};
