9b44ac9d1f3e5a9502f42a5a5f90b66900518c72
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-10-24 12:24:00 +0900

    ✨ feat(CartDetail) :: 장바구니 세부 내역을 보여주는 컴포넌트 구현

---
+++ src/refactoring/components/cart/CartDetail.tsx
@@ -0,0 +1,45 @@
+import { FC } from 'react'
+import { getMaxApplicableDiscount } from '../../hooks/utils/cartUtils'
+import { CartItem } from '../../../types'
+import { Button } from '../../components'
+
+type CardDetailProps = {
+  item: CartItem
+  onClickUpdateQuantity: (productId: string, newQuantity: number) => void
+  onClickRemoveCart: (productId: string) => void
+}
+
+export const CartDetail: FC<CardDetailProps> = ({ item, onClickUpdateQuantity, onClickRemoveCart }) => {
+  const appliedDiscount = getMaxApplicableDiscount(item)
+  return (
+    <div key={item.product.id} className="flex justify-between items-center bg-white p-3 rounded shadow">
+      <div>
+        <span className="font-semibold">{item.product.name}</span>
+        <br />
+
+        <span className="text-sm text-gray-600">
+          {item.product.price}원 x {item.quantity}
+          {appliedDiscount > 0 && (
+            <span className="text-green-600 ml-1">({(appliedDiscount * 100).toFixed(0)}% 할인 적용)</span>
+          )}
+        </span>
+      </div>
+
+      <div>
+        <Button
+          size="small"
+          color="info"
+          onClick={() => onClickUpdateQuantity(item.product.id, item.quantity - 1)}
+          text="-"
+        />
+        <Button
+          size="small"
+          color="info"
+          onClick={() => onClickUpdateQuantity(item.product.id, item.quantity + 1)}
+          text="+"
+        />
+        <Button size="small" color="error" onClick={() => onClickRemoveCart(item.product.id)} text="삭제" />
+      </div>
+    </div>
+  )
+}
