b93f13a94aae01c27672eed4d8ff084f935432f5
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-10-24 12:16:28 +0900

    ✨ feat(PaymentResult) :: 최종 결제한 결과를 보여주는 컴포넌트 구현

---
+++ src/refactoring/components/cart/PaymentResult.tsx
@@ -0,0 +1,20 @@
+import { FC } from 'react'
+import { formatKrPrice } from '../../hooks/utils'
+
+type PaymentResultProps = {
+  totalBeforeDiscount: number
+  totalAfterDiscount: number
+  totalDiscount: number
+}
+
+export const PaymentResult: FC<PaymentResultProps> = ({ totalBeforeDiscount, totalAfterDiscount, totalDiscount }) => {
+  return (
+    <div className="space-y-1">
+      <p>상품 금액: {totalBeforeDiscount.toLocaleString()}원</p>
+
+      <p className="text-green-600">할인 금액: {formatKrPrice(totalDiscount)}원</p>
+
+      <p className="text-xl font-bold">최종 결제 금액: {formatKrPrice(totalAfterDiscount)}원</p>
+    </div>
+  )
+}
