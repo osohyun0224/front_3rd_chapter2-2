5a390a69a274d90900289712cbde3a9a3be3b492
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-10-24 11:47:02 +0900

    ✨ feat(ProductItem) :: 상품 아이템 하나의 ProductItem 컴포넌트 구현

---
+++ src/refactoring/components/admin/ProductItem.tsx
@@ -0,0 +1,26 @@
+import { Product } from '../../../types'
+import { FC, ReactNode } from 'react'
+import { ItemListTemplate } from '../templates'
+import { LabelButton } from "../atoms"
+
+type ProductItemProps = {
+  product: Product
+  index: number
+  toggleProducts: (id: string) => void
+  children: ReactNode
+}
+
+export const ProductItem: FC<ProductItemProps> = ({ product, index, toggleProducts, children }) => {
+  return (
+    <ItemListTemplate>
+      <div data-testid={`product-${index + 1}`} className="bg-white p-4 rounded shadow">
+        <LabelButton
+          testId="toggle-button"
+          onClick={() => toggleProducts(product.id)}
+          label={`${product.name} - ${product.price}원 (재고: ${product.stock})`}
+        />
+        {children}
+      </div>
+    </ItemListTemplate>
+  )
+}
